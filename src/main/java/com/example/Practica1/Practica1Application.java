acdff18112acc5453fedbdba8d935eb88f582106
Cristian Labra Castro <clabca@gmail.com>
2023-06-28 21:54:04 -0400

    Primer COmmit

---
+++ src/main/java/com/example/Practica1/Practica1Application.java
@@ -0,0 +1,14 @@
+package com.example.Practica1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1Application.class, args);
+		System.out.println("Esto es un Hola Mundo !!!! ");
+	}
+
+}
